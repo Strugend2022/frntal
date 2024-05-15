06c9059970492dcd65bafed369e702b078fed760
Triyambaka Mishra <triyambakam2@gmail.com>
2024-05-15 12:08:01 +0530

    added favicon

--- src/pages/home.js
+++ src/pages/home.js
@@ -39,7 +39,7 @@ const HomePage = () => {
   return (
     <>
       <Head>
-        <title>Real Estate </title>
+        <title>Frntal</title>
         <meta name="description" content="Struggling to find employee housing? Frntal simplifies corporate relocations & temporary stays. We find furnished apartments & homes for your team, ensuring a smooth transition. Stress-free rentals, happy employees." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title> Frntal </title>
+        <title>Frntal</title>
         <meta name="description" content="Struggling to find employee housing? Frntal simplifies corporate relocations & temporary stays. We find furnished apartments & homes for your team, ensuring a smooth transition. Stress-free rentals, happy employees." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
